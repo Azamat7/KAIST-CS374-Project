c0076712d960b0e08e5039bdff899b1d83fe1744
Kevin Min <placidmoon1@gmail.com>
2018-05-17 23:03:17 +0900

    Add files via upload

--- Mapping the World/public/event-create/event-create.js
+++ Mapping the World/public/event-create/event-create.js
@@ -33,6 +33,17 @@ $(document).ready(function() {
     $("#time").val("");
     $("#location").val("");   
     $("input[name=inlineRadioOptions]:checked").val("");
+    showSnackbar();
   });
-
+  function showSnackbar() {
+    // Get the snackbar DIV
+    var x = document.getElementById("snackbar");
+  
+    // Add the "show" class to DIV
+    x.className = "show";
+  
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+  }
+  
 });
--- Mapping the World/public/events/events.js
+++ Mapping the World/public/events/events.js
@@ -13,20 +13,48 @@ $(document).ready(function(){
     for ( var i = 0; i < keyVal.length; i++) {
       let eventObject = dbSnapshot[keyVal[i]];
       let html = 
-      '<div class = "list-group-item list-group-secondary">' +
-        '<h5 class = "mb-1">'+ eventObject["title"]+ '<i class = "fa fa-map-o"></i><small>    ' + eventObject["type"]+  '</small></h5>' + 
+      '<div class = "list-group-item list-group-secondary ' +eventObject["type"] + '">' +
+        '<h5 class = "mb-1">'+ eventObject["title"] + " " + "<small>" + eventObject["type"]+  '</small></h5>' + 
         '<small class = "mb-4"> By: ' + eventObject["organizer"] + '</small>' +
         '<p class = "mt-2">' + eventObject["description"] + '</p>' +
         '<div class = "d-flex justify-content-between">' +
           '<span>' + eventObject["time"] + '</span> <span>' + eventObject["location"] + ' </span> ' +
         '</div>' + 
+        '<a class = "mt-3 btn btn-outline-info" href = "#" style = "font-size: 12px;"> Find in Map </a>'
       '</div>';
       $("#list-group-append").append(html);
     }
   });
-  $(document).on('click', '#inlinecheckbox1',function(){
+  $(document).on('click', '#inlineCheckbox1',function(){
     $(".list-group-item").each(function(){
-			$(this).show();
+      if ($(this).hasClass("#Study")) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+		});
+  }); 
+  $(document).on('click', '#inlineCheckbox2',function(){
+    $(".list-group-item").each(function(){
+      if ($(this).hasClass("#Club")) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+		});
+  }); 
+  $(document).on('click', '#inlineCheckbox3',function(){
+    $(".list-group-item").each(function(){
+      if ($(this).hasClass("#Fun")) {
+        $(this).show();
+      } else {
+        $(this).hide();
+      }
+		});
+  }); 
+  $(document).on('click', '#inlineCheckbox4',function(){
+    $(".list-group-item").each(function(){
+      $(this).show();
 		});
   }); 
 });
